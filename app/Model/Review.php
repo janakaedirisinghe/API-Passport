206b674168ab9f8b098e2df579824c216d1d98ca
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-13 20:18:24 +0530

    auth check for destroy

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = [
+        'star','customer','review'
+    ];
+
    public function product(){
+
+
        return $this->belongsTo(Product::class);
    }
 }
