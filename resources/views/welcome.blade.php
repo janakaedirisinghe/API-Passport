ee0a4586e5f5802534b9c668a7528e775c17b2da
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-13 23:58:57 +0530

    auth check for destroy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,21 +139,25 @@
         </div>
     </div>
     <br>
+    <hr>
 
-        <h3>Routes</h3>
+
+        <h3>All Routes</h3>
         <br>
+    <div class="row" style="align-items: center">
         <pre>
-        | POST      | api/products                            |
-        | GET|HEAD  | api/products                            |
-        | GET|HEAD  | api/products/{product}                  |
-        | DELETE    | api/products/{product}                  |
-        | PUT|PATCH | api/products/{product}                  |
-        | POST      | api/products/{product}/reviews          |
-        | GET|HEAD  | api/products/{product}/reviews          |
-        | DELETE    | api/products/{product}/reviews/{review} |
-        | PUT|PATCH | api/products/{product}/reviews/{review} |
+        | POST      | api/products                            | insert products | authentication is required
+        | GET|HEAD  | api/products                            | get all products
+        | GET|HEAD  | api/products/{product}                  | get single product
+        | DELETE    | api/products/{product}                  | delete single product | authentication is required
+        | PUT|PATCH | api/products/{product}                  | update your single product | authentication is required
+        | POST      | api/products/{product}/reviews          | post review belong to product | authentication is required
+        | GET|HEAD  | api/products/{product}/reviews          | get product reviews
+        | DELETE    | api/products/{product}/reviews/{review} | delete single review | authentication is required
+        | PUT|PATCH | api/products/{product}/reviews/{review} | update your single review | authentication is required
 
-            </pre>
+        </pre>
+    </div>
 
     <hr>
 
